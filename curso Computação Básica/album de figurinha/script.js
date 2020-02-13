e1d45b1e4c894fa132f6266050e924fac00aa34f
only_ros3 <rosefelix99@gmail.com>
2020-02-13 15:22:01 -0300

    leiame

---
+++ curso Computação Básica/album de figurinha/script.js
@@ -0,0 +1,17 @@
+var colecao = [1, 5, "b", "A", new Image()];
+
+console.log(colecao.length); //Mostra tamanho do array
+
+colecao.push(10); //Adiciona item no final do array
+
+console.log(colecao); //Mostra toda coleção
+
+colecao.pop(); //Excluí ultimo item do array
+
+console.log(colecao);
+
+console.log(colecao[0]);
+
+console.log(colecao[2]);
+
+console.log(colecao[colecao.length-1]);
