405ffeee23f2165444d7858279eeca6d367b2fbe
rosinele <rosefelix99@gmail.com>
2020-02-29 01:30:57 -0300

    Nome da pasta do Curso 01

--- 01 - Computação Básica/conhecendo objetos/script.js
+++
@@ -1,8 +0,0 @@
-console.log("estou no inicio do script")
-
-document.write("Esta é uma linha");
-document.writeln("Está é uma linha com writeln");
-document.write("Outra linha");
-
-//window.location.reload();
-console.log("cheguei no fim do script")
