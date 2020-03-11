bd273670117be8cc39a80dd7320e3db0db836ccf
rosinele <rosefelix99@gmail.com>
2020-03-11 19:43:23 -0300

    finalização do projeto com configuração do ng-app

---
+++ 06 - Responsive Design/angularJS/login.js
@@ -0,0 +1,9 @@
+var app = angular.module('loginApp',['ngMaterial','ngMessages']);
+
+app.controller('MainCtrl', ['$scope', function($scope){
+    $scope.loginModel = {
+        userName: '',
+        password: ''
+    };
+    
+}]);
