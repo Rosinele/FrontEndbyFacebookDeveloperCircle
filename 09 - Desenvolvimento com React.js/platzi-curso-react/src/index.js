5a23be48b14c6d08b6bc3b71af34e6833d6921a7
rosinele <rosefelix99@gmail.com>
2020-03-17 23:16:19 -0300

    Componentes de função de e de classe

--- 09 - Desenvolvimento com React.js/platzi-curso-react/src/index.js
+++ 09 - Desenvolvimento com React.js/platzi-curso-react/src/index.js
@@ -1,14 +1,45 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; //Renderizar os componentes no DOM (Existem também o react nativa, react-canvas)
 
-const elementoComJSX = (
+/*const ElementoComJSX = (props) => (
     <div className="platzi" autoCorrect>
-        <h1> Olá, estamos utilizando o JSX agora!</h1>
+        <h1>{props.texto}</h1>
     </div>
 );
 
 ReactDOM.render(
-    elementoComJSX, 
+    <div>
+        <ElementoComJSX texto="Olá, Rose! Fizemos um Componente agora"/>
+    <ElementoComJSX texto="Olha só, estou reutilizando um componente"/> 
+    </div>,
+    document.getElementById('root')
+);*/
+
+//Componente funcional
+const Oi = (props) => (
+    <h1>Olá, {props.nome}</h1>
+);
+
+//componente de classe (Todo componente de classe tem que ter a função render() para devolver alguma coisa)
+class Tchau extends React.Component{
+    render(){
+    return <h3>Tchau {this.props.nome}</h3>
+    }
+}
+
+const nome = "Rose";
+const nome2 = "Maria";
+const nome3 = "Victor";
+
+ReactDOM.render(
+    <div>
+        <Oi nome={nome}/>
+        <Tchau nome={nome}/>
+        <Oi nome={nome2}/>
+        <Tchau nome={nome2}/>
+        <Oi nome={nome3}/>
+        <Tchau nome={nome3}/>
+    </div>,
     document.getElementById('root')
 );
 
