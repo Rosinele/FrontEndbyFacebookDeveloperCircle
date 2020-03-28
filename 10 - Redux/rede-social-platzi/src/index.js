d852206ca61729bbae7f80ec3722729e5140c109
rosinele <rosefelix99@gmail.com>
2020-03-28 16:10:29 -0300

    redux

--- 10 - Redux/rede-social-platzi/src/index.js
+++ 10 - Redux/rede-social-platzi/src/index.js
@@ -1,45 +1,76 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
 
-import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
-import {Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+const APP_CONTAINER_NAME = 'app-container';
 
 const App = () => (
-    <Provider store = {store}>
-        <ROuter>
-          <div>
-            <Route
-              exact
-              path="/"
-              render = {() => (
-                requireAuthentication()
-                ? <Redirect to = "/login"/>
-                : <FeedCOntainer/>
-              )}>
-            </Route>
-            <Route
-              exact
-              path="/"
-              render = {() => (
-                requireAuthentication()
-                ? <Redirect to = "/login"/>
-                : <FeedCOntainer/>
-              )}>
-            </Route>
-            <Route
-              exact
-              path="/"
-              render = {() => (
-                requireAuthentication()
-                ? <Redirect to = "/login"/>
-                : <FeedCOntainer/>
-              )}>
-            </Route>
-          </div>
-        </ROuter>
-    </Provider>
-)
-ReactDOM.render(<App />,  document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            requireAuthentication()
+              ? <Redirect to="/login" />
+              : <FeedContainer />
+          )}
+        />
+        <Route
+          exact
+          path="/register"
+          render={() => (
+            requireAuthentication()
+              ? <Redirect to="/" />
+              : <RegisterContainer />
+          )}
+        />
+        <Route
+          exact
+          path="/login"
+          render={() => (
+            requireAuthentication()
+              ? <Redirect to="/" />
+              : <LoginContainer />
+          )}
+        />
+        <Route
+          exact
+          path="/user/:user_id"
+          render={props => (
+            requireAuthentication()
+              ? <Redirect to="/login" />
+              : <ProfilerContainer {...props} />
+          )}
+        />
+        <Route
+          exact
+          path="/post/:post_id"
+          render={props => (
+            requireAuthentication()
+              ? <Redirect to="/login" />
+              : <PostContainer />
+          )}
+        />
+      </div>
+    </Router>
+  </Provider>
 );
+
+window.addEventListener('DOMContentLoaded', () => {
+  let appContainer = document.getElement(APP_CONTAINER_NAME);
+
+  if (!appContainer) {
+    appContainer = document.createElement('DIV');
+    appContainer.id = APP_CONTAINER_NAME;
+    document.body.appendChild(appContainer);
+  }
+
+  render(<App />, appContainer);
+});
+
 
